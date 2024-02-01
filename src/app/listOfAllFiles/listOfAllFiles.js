4c61c79bde0ade04b9b186caca6901d70f636644
l1senochek <lisenochek96@gmail.com>
2024-02-01 15:10:19 +0300

    feat: add sort in alphabetical order ascending (folders goes first)

--- src/app/listOfAllFiles/listOfAllFiles.js
+++ src/app/listOfAllFiles/listOfAllFiles.js
@@ -6,19 +6,27 @@ const listOfAllFiles = async () => {
   try {
     const currentDirectory = process.cwd();
     const allFiles = await fs.readdir(currentDirectory);
-    allFiles.sort();
 
-    console.log('List of all files:');
-
-    const tableData = await Promise.all(
+    const files = await Promise.all(
       allFiles.map(async (file) => {
         const fullPath = path.join(currentDirectory, file);
-        const stats = await fs.stat(fullPath);
-        const type = stats.isDirectory() ? 'Folder' : 'File';
-        return { Type: type, Name: file };
+        const stat = await fs.stat(fullPath);
+        return { name: file, isDirectory: stat.isDirectory() };
       })
     );
 
+    const sortedFiles = files.sort((a, b) => {
+      if (a.isDirectory && !b.isDirectory) return -1;
+      if (!a.isDirectory && b.isDirectory) return 1;
+      return a.name.localeCompare(b.name);
+    });
+
+    const tableData = sortedFiles.map((entry) => ({
+      Type: entry.isDirectory ? 'Folder' : 'File',
+      Name: entry.name,
+    }));
+
+    console.log('List of all files:');
     console.table(tableData);
   } catch (error) {
     console.log(colorize('Operation failed', 'red'));
