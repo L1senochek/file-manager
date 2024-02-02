08e844102ee5caaab2492702b6ed0e02c044a834
l1senochek <lisenochek96@gmail.com>
2024-02-02 11:45:15 +0300

    feat: update add.js

--- src/app/commandsBasic/add/add.js
+++ src/app/commandsBasic/add/add.js
@@ -1,15 +1,21 @@
 import fs from 'fs';
+import path from 'path';
 import colorize from '../../utils/colorize.js';
 
 const add = async (fileName) => {
   try {
     const currentDirectory = process.cwd();
     const filePath = path.join(currentDirectory, fileName);
+    const content = '';
 
-    fs.writeFile(filePath, content, { flag: 'wx' });
-
-    console.log(colorize(`File created successfully.`, 'green'));
-  } catch {
+    fs.writeFile(filePath, content, { flag: 'wx' }, (error) => {
+      if (error) {
+        console.log(colorize('Operation failed', 'red'));
+      } else {
+        console.log(colorize(`File created successfully.`, 'green'));
+      }
+    });
+  } catch (error) {
     console.log(colorize('Operation failed', 'red'));
   }
 };
