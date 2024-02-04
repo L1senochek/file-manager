371baa9aa891107f1fddbad966fd31568d5ecca5
L1senochek <lisenochek96@gmail.com>
2024-02-04 17:23:35 +0300

    refactor: clear console.log in cat.js file

--- src/app/commandsBasic/cat/cat.js
+++ src/app/commandsBasic/cat/cat.js
@@ -3,16 +3,12 @@ import colorize from '../../utils/colorize.js';
 
 const cat = async (filePath) => {
   try {
-    console.log(filePath);
     filePath.startsWith("'") || filePath.startsWith('"')
       ? (filePath = filePath.slice(1))
       : null;
-
     filePath.endsWith("'") || filePath.endsWith('"')
       ? (filePath = filePath.slice(0, -1))
       : null;
-
-    console.log(filePath);
 
     const readStream = fs.createReadStream(filePath, {
       encoding: 'utf-8',
