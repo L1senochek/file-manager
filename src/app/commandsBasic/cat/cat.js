cbf88f2efeea2e9d19ecced0dcb185f62fc5af58
l1senochek <lisenochek96@gmail.com>
2025-05-05 19:14:49 +0300

    fix: fix cat result output

--- src/app/commandsBasic/cat/cat.js
+++ src/app/commandsBasic/cat/cat.js
@@ -4,20 +4,30 @@ import { colorize } from "../../utils/index.js";
 const cat = async (filePath) => {
   try {
     filePath.startsWith("'") || filePath.startsWith('"')
-      ? (filePath = filePath.slice(1))
-      : null;
+        ? (filePath = filePath.slice(1))
+        : null;
     filePath.endsWith("'") || filePath.endsWith('"')
-      ? (filePath = filePath.slice(0, -1))
-      : null;
+        ? (filePath = filePath.slice(0, -1))
+        : null;
 
     const readStream = fs.createReadStream(filePath, {
       encoding: 'utf-8',
     });
 
-    readStream.on('data', (chunk) => process.stdout.write(chunk));
-    readStream.on('error', () => {
-      console.log(colorize('Operation failed', 'red'));
-    });
+    return new Promise((resolve) => {
+      let content = ''
+      readStream.on('data', (chunk) => {
+        content += chunk;
+      });
+      readStream.on('end', () => {
+        process.stdout.write(content);
+        resolve(content);
+      })
+      readStream.on('error', () => {
+        console.log(colorize('Operation failed', 'red'));
+        resolve();
+      });
+    })
   } catch {
     console.log(colorize('Operation failed', 'red'));
   }
